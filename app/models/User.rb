89a98b0b9bdd5128fd73e2c18e8b2155a54a944a
github username <github email address>
2019-02-05 11:59:45 -0500

    stuff

---
+++ app/models/User.rb
@@ -0,0 +1,39 @@
+class User
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def recipes
+    RecipeCard.all.select do |recipecard|
+      recipecard.user == self
+    end.map do |recipecard|
+      recipecard.recipe
+    end
+  end
+
+  def allergens
+    Allergen.all.select do |allergy|
+      allergy.user == self
+    end.map do |allergy|
+      allergy.ingredient
+    end
+  end
+
+  def add_recipe_card(recipe, date, rating)
+    # new_recipe = Recipe.new('oreo cake')
+    RecipeCard.new("#{recipe.name} recipe", date, rating, recipe, self)
+  end
+
+  def declare_allergen(ingredient)
+    Allergen.new("#{ingredient.name} allergy", ingredient, self)
+  end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,36 @@
 require_relative '../config/environment.rb'
 
+user1 = User.new('Myung')
+user2 = User.new('Sharon')
+user3 = User.new('Greg')
+
+recipe1 = Recipe.new('strawberry cake')
+recipe2 = Recipe.new('cheese cake')
+recipe3 = Recipe.new('banana cake')
+recipe4 = Recipe.new('pudding cake')
+
+recipecard1 = RecipeCard.new('s-c recipe', '06-09-1995', '10', recipe1, user1)
+recipecard2 = RecipeCard.new('c-c recipe', '04-04-1992', '8', recipe2, user2)
+recipecard3 = RecipeCard.new('b-c recipe', '02-05-2019', '4', recipe3, user3)
+recipecard4 = RecipeCard.new('p-c recipe', '02-06-2019', '5', recipe4, user1)
+
+ingredient1 = Ingredient.new('flour')
+ingredient2 = Ingredient.new('sugar')
+ingredient3 = Ingredient.new('egg')
+
+recipeingredient1 = RecipeIngredient.new('s-c ingredients', ingredient1, recipe1)
+recipeingredient2 = RecipeIngredient.new('c-c ingredients', ingredient2, recipe2)
+recipeingredient3 = RecipeIngredient.new('b-c ingredients', ingredient3, recipe3)
+
+allergy1 = Allergen.new('strawberry allergy', ingredient1, user1)
+allergy2 = Allergen.new('lactose allergy', ingredient2, user2)
+allergy3 = Allergen.new('potassium allergy', ingredient3, user3)
+
 binding.pry
+
+puts user1
+puts recipe1
+puts recipecard1
+puts ingredient2
+puts recipeingredient2
+puts allergy2
