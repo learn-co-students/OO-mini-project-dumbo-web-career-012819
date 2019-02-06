929fa7077d493068c75d5e9e1228c55d9cfabedf
github username <github email address>
2019-02-05 19:40:21 -0500

    Done.

--- app/models/User.rb
+++ app/models/User.rb
@@ -40,9 +40,30 @@ class User
   # - `User#top_three_recipes`
   # should return the top three highest rated recipes for this user.
   def top_three_recipes
-    RecipeCard.all.sort do |x, y|
+    top_three = []
+
+    sorted_cards = RecipeCard.all.sort do |x, y|
       x.rating <=> y.rating
     end
+
+    3.times do
+      top_three << sorted_cards.pop
+    end
+
+    top_three
   end
 
+  def most_recent_recipe
+    recipes.pop
+  end
+
+  #User safe_recipes should return all recipes that do not contain ingredients the user is allergic to
+  def safe_recipes
+    safe_list = []
+
+    RecipeIngredient.all.each do |recipeingredient|
+        safe_list << recipeingredient.recipe unless allergens.include?(recipeingredient.ingredient)
+      end
+    safe_list
+  end
 end
